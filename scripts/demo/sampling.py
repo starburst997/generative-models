dfe83836e03298028520eae0d4cd2b915772459d
Jean-Denis Boivin <jd@boiv.in>
2023-08-10 11:30:19 -0400

    Fix install / demo

--- scripts/demo/sampling.py
+++ scripts/demo/sampling.py
@@ -1,3 +1,6 @@
+import os, sys 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '../..')))
+
 from pytorch_lightning import seed_everything
 
 from scripts.demo.streamlit_helpers import *
